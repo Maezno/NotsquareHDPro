af165b916aefab4de5bb676b6fbb56850bbd3563
Maezno <maezno@gmail.com>
2025-02-24 15:25:09 -0300

    Actualización de contenido

---
+++ buildSrc/src/main/java/MyApp.kt
@@ -0,0 +1,7 @@
+@file:Suppress("unused")
+
+object MyApp {
+    const val appId = "dev.jahir.blueprint.app"
+    const val version = 243
+    const val versionName = "2.4.3"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,19 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.6.1"
+    const val kotlin = "2.1.0"
+    const val ksp = "$kotlin-1.0.29"
+
+    // OneSignal
+    const val oneSignal = "4.8.10"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 35
+    const val buildTools = "35.0.0"
+
+    // Blueprint
+    const val blueprint = "2.4.3"
+}
